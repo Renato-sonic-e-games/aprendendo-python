e5e874ab75f4275bea265685a8248eda2905a49c
Renato-sonic-e-games <rennerutame009@gmail.com>
2025-08-27 12:18:58 -0300

    aula 2

--- arquivoarquivadoarquivalquearquivaarquivando.py
+++ arquivoarquivadoarquivalquearquivaarquivando.py
@@ -1,18 +1,12 @@
+#primeira aula
 conta = input('qual é seu nome?')
 idade = input('qual é sua idade?')
 altura = input ('qual é sua altura?')
 imprego = input ('você trabalha?(digite S para sim e N para não)')
-if imprego =='S': emprego == True
-elif imprego =='N': emprego == False
-else: 'sei lá, tu digitou errado aí, doido'
-#one hundred cigarretes
-
-
-#exibando uns coiso aí eu acho
 print ('>:D')
 print('a soma é:', 2+3)
 print ('a multiplacação é{}'.format (5*5))
 print (f'a divisão é {10/2}')
 print(f'seu nome é {conta}, sua idade é {idade}, sua altura é', altura, "e sua situação é de")
-if emprego == True: print ('emprego')
+if imprego == "S": print ('emprego')
 else: print ('desemprego')
---
+++ aula02.py
@@ -0,0 +1,34 @@
+# # operadores que operam operando em operações operativas
+
+# numero = int(input("digite um número: "))
+# nmr02 = int(input("digite outro número aí: "))
+
+# soma = numero + nmr02
+# subtracao = numero - nmr02
+# multi = numero * nmr02
+# div = numero / nmr02
+# mod = numero % nmr02
+# divisaointeira = numero // nmr02
+# potenciacao = numero ** nmr02
+# raiz = pow(numero, 0.5)
+
+# print('a soma é: ', soma)
+# print('a subtração é: ', subtracao)
+# print('a multiplicação é: ', multi)
+# print('a divisão é: ', div)
+# print('a sobra da divisão é: ', mod)
+# print('a divisão inteira é: ', divisaointeira)
+# print('a potenciação é: ', potenciacao)
+# print ('a raiz quadrada é: ', raiz)
+
+#Funções condicionais
+
+print ('~~~~~~~~~~~~~~~~~~~')
+print ('Seja Mal Vindo - >:D MUAHAHAHA! - ao sistema de horas :0')
+print('===================')
+horaPirata = int(input('Digite um horário: '))
+if horaPirata < 12:
+    print('Bom dia!')
+elif horaPirata <18: 
+    print('Boa Tarde!')
+else: print('Boa noite!')
