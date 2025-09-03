8242e0182aafdae230ce8edf7ff23ce2e8b70b41
renato-sonic-e-games <rennerutame009@gmail.com>
2025-09-03 12:58:39 -0300

    bla bla bla

---
+++ aula_03.py
@@ -0,0 +1,10 @@
+# oqueousuarioquiserdigitar = int(input("digita aí um número doido: "))
+
+# while oqueousuarioquiserdigitar != 0:
+#     print ("o valor digitado é ", oqueousuarioquiserdigitar)
+#     oqueousuarioquiserdigitar = int(input("digita aí um número doido: "))
+tabulada = int(input("digite a tabuada de algum número INFINITAMENTE MUAHAHAHA(ou até você digitar 0): "))
+while tabulada != 0:
+    for contador in range(1, 11):
+            print(tabulada," x ", contador," = ",contador*tabulada)
+    tabulada = int(input("digite a tabuada de algum número INFINITAMENTE MUAHAHAHA(ou até você digitar 0): "))
